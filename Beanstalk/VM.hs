d7bdf7d51c0bdf08fa5181ff1cb2141d348d02c2
James Halliday <substack@gmail.com>
2010-01-11 00:52:58 -0900

    newVM with getScreenUpdate refactoring

--- Beanstalk/VM.hs
+++ Beanstalk/VM.hs
@@ -1,5 +1,6 @@
 module Beanstalk.VM (
-    VM(..), updateThread
+    VM(..), UpdateData(..), DrawData(..),
+    updateThread, getScreenPng, newVM
 ) where
 
 import qualified Graphics.GD as GD
@@ -36,34 +37,50 @@ data DrawData = DrawData {
     drawBytes :: Int
 }
 
+newVM :: RFB -> IO VM
+newVM rfb = do
+    updates <- atomically $ newTMVar []
+    screen <- atomically . newTMVar =<< getScreenUpdate rfb 0
+    latest <- atomically $ newTMVar 0
+    
+    return $ VM {
+        vmRFB = rfb,
+        vmUpdates = updates,
+        vmScreen = screen,
+        vmLatestUpdate = latest
+    }
+
+-- get the screen png if it needs to be gotten
 getScreenPng :: VM -> IO BL.ByteString
 getScreenPng vm = do
-    updateScreenPng vm
-    drawPng . head . updateData <$> (atomically $ readTMVar $ vmScreen vm)
-
--- update the screen png data if it needs updating
-updateScreenPng :: VM -> IO ()
-updateScreenPng vm = do
     screen <- atomically $ readTMVar $ vmScreen vm
     let screenV = updateVersion screen
     updateV <- atomically $ readTMVar $ vmLatestUpdate vm
     
-    when (screenV /= updateV) $ do
-        let tm = vmScreen vm
-        atomically $ takeTMVar tm
-        im <- getImage $ vmRFB vm
-        png <- repack <$> GD.savePngByteString im
-        let bytes = fromIntegral $ BL.length png
-        atomically $ putTMVar tm $ UpdateData {
-            updateBytes = bytes,
-            updateVersion = updateV,
-            updateData = (:[]) $ DrawData {
-                drawPng = png,
-                drawPos = (0,0),
-                drawSize = fbWidth &&& fbHeight $ rfbFB $ vmRFB vm,
-                drawBytes = bytes
-            }
+    if screenV == updateV
+        then return $ drawPng $ head $ updateData screen
+        else do
+            let tm = vmScreen vm
+            update <- getScreenUpdate (vmRFB vm) updateV
+            atomically $ swapTMVar tm update
+            return $ drawPng $ head $ updateData update
+
+-- get the screen as an UpdateData
+getScreenUpdate :: RFB -> Int -> IO UpdateData
+getScreenUpdate rfb version = do
+    im <- getImage rfb
+    png <- repack <$> GD.savePngByteString im
+    let bytes = fromIntegral $ BL.length png
+    return $ UpdateData {
+        updateBytes = bytes,
+        updateVersion = version,
+        updateData = (:[]) $ DrawData {
+            drawPng = png,
+            drawPos = (0,0),
+            drawSize = fbWidth &&& fbHeight $ rfbFB rfb,
+            drawBytes = bytes
         }
+    }
 
 updateThread :: VM -> IO ()
 updateThread vm = forever $ do
