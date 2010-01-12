36ec763caf56a45b3001aa3870f08cf0c5a9ed7b
James Halliday <substack@gmail.com>
2010-01-11 21:17:56 -0900

    blocking upload timeouts, but flashy redrawing persists

--- Main.hs
+++ Main.hs
@@ -10,6 +10,8 @@ import Control.Concurrent.STM.TMVar
 
 import Data.Maybe (fromJust)
 import Control.Applicative ((<$>))
+import Control.Monad (msum, mzero)
+
 import Data.ByteString.Lazy.Char8 (pack)
 import qualified Text.JSON as JS
 
@@ -36,12 +38,20 @@ beanstalkRoutes vm = do
     
     get "/api/console/get_update_list/:version" $ do
         versionId <- read <$> fromJust <$> capture "version"
-        updates <- io $ getUpdates vm versionId
+        -- loop until updates are available
+        updates <- io $ msum $ repeat $ do
+            u <- getUpdates vm versionId
+            if null $ updateData u
+                then mzero
+                else return u
+        
         let updateCount = length $ updateData updates
         let latestVersion = updateVersion updates
+        io $ print updateCount
+        
         with_type "text/javascript"
         with_body $ pack $
-            if updateCount > 200
+            if updateCount > 100
                 then JS.encode [[latestVersion,-1]]
                 else JS.encode $ [[latestVersion,updateCount]]
                     ++ [ [
--- static/js/console.js
+++ static/js/console.js
@@ -1,20 +1,22 @@
 function Console(console_elem) {
     function get_screen() {
+        var im = $(document.createElement("img"))
+            .attr("src", "/api/console/get_screen"
+                + "?" + String(Math.floor(Math.random() * 1e10))
+            )
+            .css({
+                position : "absolute",
+                left : 0, top : 0
+            });
         console_elem
             .empty()
-            .append(
-                $(document.createElement("img"))
-                    .attr("src", "/api/console/get_screen")
-                    .css({
-                        position : "absolute",
-                        left : 0, top : 0
-                    })
-            );
+            .append(im);
     }
     
     function get_update(args) {
         var uri = "/api/console/get_update/"
             + String(args.version_id) + "/" + String(args.update_id);
+            + "?" + String(Math.floor(Math.random() * 1e10));
         var im = $(document.createElement("img"))
             .attr("src", uri)
             .css({
@@ -28,28 +30,38 @@ function Console(console_elem) {
     }
     
     function get_updates(version_id) {
-        var uri = "/api/console/get_update_list/" + String(version_id);
-        $.getJSON(uri, function (data) {
-            var latest_version = data[0][0];
-            var update_count = data[0][1];
-            if (update_count < 0) {
-                get_screen();
-            }
-            else {
-                for (var i = 1; i < data.length; i++) {
-                    var item = data[i];
-                    console.log(item.join(","));
-                    get_update({
-                        version_id : version_id,
-                        update_id : i - 1,
-                        x : item[0],
-                        y : item[1],
-                        width : item[2],
-                        height : item[3]
-                    });
+        var uri = "/api/console/get_update_list/" + String(version_id)
+            + "?" + String(Math.floor(Math.random() * 1e10));
+        $.ajax({
+            url : uri,
+            dataType : "json",
+            cache : false,
+            success : function (data) {
+                var latest_version = data[0][0];
+                var update_count = data[0][1];
+                if (update_count < 0) {
+                    get_screen();
                 }
-            }
-            setTimeout(function () { get_updates(latest_version) }, 500);
+                else {
+                    for (var i = 1; i < data.length; i++) {
+                        var item = data[i];
+                        console.log(item.join(","));
+                        get_update({
+                            version_id : version_id,
+                            update_id : i - 1,
+                            x : item[0],
+                            y : item[1],
+                            width : item[2],
+                            height : item[3]
+                        });
+                    }
+                }
+                get_updates(latest_version);
+            },
+            error : function () {
+                get_updates(version_id);
+            },
+            timeout : 3000,
         });
     };
     
