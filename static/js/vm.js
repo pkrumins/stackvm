dbd5da9c1e992ecd26e2c980bc1638b93f67fd38
Peteris Krumins <peteris.krumins@gmail.com>
2010-04-23 10:38:22 -0400

    update screen works via base64 encoded pngs

--- static/js/vm.js
+++ static/js/vm.js
@@ -17,16 +17,23 @@ var VM_Manager = (function() {
 function VM_Event_Handler(vm) {
   this.vm = vm;
 
-  this.connected = function() {
-    // TODO: ...
+  this.error = function(msg) {
+    console.log("Error: " + msg.find('error').text());
   }
 
-  this.start_vm = function(msg) {
-    // TODO: display hello screen
+  this.connect = function(msg) {
+    console.log('stackvm connected');
   }
 
   this.update_screen = function(msg) {
-    // TODO: update screen
+    console.log('update_screen');
+    var png = msg.find('png').text();
+    var img = $('<img>').attr('src', 'data:image/png;base64,' + png);
+    vm.win.append(img);
+  }
+
+  this.disconnect = function(msg) {
+    console.log('stackvm disconnected');
   }
 }
 
@@ -38,14 +45,15 @@ function VM_Event_Emitter(vm) {
   }
 
   this.start_vm = function() {
-    XMPP.send_msg( 
+    XMPP.send_msg(
         prepare_msg().
         c('action').t('start_vm')
     );
   }
   this.send_key_down = function(key_code) {
     // TODO: see the flush() method in strophe.js, seems like
-    // strophe.js queues events up to 100ms, and we can't have 100ms lag for sending each key!
+    // strophe.js queues events up to 100ms, and we can't have 100ms
+    // lag for sending each key!
     XMPP.send_msg(
         prepare_msg().
         c('action').t('key_down').up().
@@ -62,33 +70,40 @@ function VM_Event_Emitter(vm) {
 }
 
 function VM(vm_id) {
-  this.vm_id = vm_id;
-  event_emitter = new VM_Event_Emitter(this);
+  this.vm_id  = vm_id;
+  this.win = null;
+  var event_emitter = new VM_Event_Emitter(this);
 
   function create_window() {
-    $('#content').append(
-      $('<div>').
-        data('vm_id', vm_id).
-        attr('id', vm_id + '_window').
-        attr('class', 'window').
-        height(400). // while testing
-        width(400)
-    );
+    var win = $('<div>').
+                 data('vm_id', vm_id).
+                 attr('id', vm_id + '_window').
+                 attr('class', 'window').
+                 height(400). // while testing
+                 width(400);
+    $('#content').append(win);
+    win.keydown(function(ev) {
+        alert('foo');
+      event_emitter.send_key_down(ev.keyCode);
+    });
+    win.keyup(function(ev) {
+      event_emitter.send_key_up(ev.keyCode);
+    });
+    return win;
   }
 
   this.run = function() {
-    create_window();
+    this.win = create_window();
     XMPP.add_event_handler(new VM_Event_Handler(this));
     event_emitter.start_vm();
   }
 
   this.send_key_down = function(key_code) {
-    // ...
+    event_emitter.send_key_down(key_code);
   }
 
   this.send_key_up = function(key_code) {
-    // ...
+    event_emitter.send_key_up(key_code);
   }
-
 }
 
