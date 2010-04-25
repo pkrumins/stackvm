62ff77620b9785661fa7148e6a113d87c04e0cf8
Peteris Krumins <peteris.krumins@gmail.com>
2010-04-25 10:12:40 -0400

    use node.js buffers. fix some nodejs 0.1.33->0.1.92 porting bugs

--- nodejs/stackvm.js
+++ nodejs/stackvm.js
@@ -5,7 +5,7 @@
 var sys  = require('sys');
 var tcp  = require('net');
 var http = require('http');
-// var buffer = require('buffer');    new in node 0.1.90
+var buffer = require('buffer');
 
 var xmpp = require('./libs/xmpp');
 var base64 = require('./libs/base64');
@@ -19,8 +19,6 @@ var vm_map = {
   linux1: {'host': '127.0.0.1', 'port': 25900},
   dos1:   {'host': '127.0.0.1', 'port': 25901}
 };
-
-xmpp_connection = null;
 
 function xmpp_connect_cb(status, error_message) {
   xmpp_server = "XMPP server at " + xmpp_host + ":" + xmpp_port;
@@ -58,62 +56,59 @@ function xmpp_connect_cb(status, error_message) {
 var stackvms = {};
 
 function handler_start_vm(vm_id, msg) {
-  if (!vm_map[vm_id]) {
-    xmpp_connection.send(
-      xmpp.message({to:msg.getAttribute('from')}).
-      c('vm_id').t(vm_id).up().
-      c('error').t("There is no VM with id '" + vm_id + "'"));
-  }
-  else {
-    /* TODO: This when we have a permanent TCP connections with stackvm
-    stackvm_socket = tcp.createConnection(vm_map[vm_id]['port'], vm_map[vm_id]['host']);
-    stackvm_socket.setTimeout(0);
-    stackvm_socket.addListener("connect", function() {
-      xmpp.message({to:msg.getAttribute('from')}).
-      c('vm_id').t(vm_id).up().
-      c('action').t('connect');
-    });
-    stackvm_socket.addListener("disconnect", function(errno) {
-      xmpp.message({to:msg.getAttribute('from')}).
-      c('vm_id').t(vm_id).up().
-      c('action').t('disconnect');
-    });
-    // stackvm_socket.addListener("data", function(data) { });
-    */
+  /*
+  ** TODO: This when we have a permanent TCP connections with stackvm
+  *
+  stackvm_socket = net.createConnection(vm_map[vm_id]['port'], vm_map[vm_id]['host']);
+  stackvm_socket.setTimeout(0);
+  stackvm_socket.addListener("connect", function() {
+    xmpp.message({to:msg.getAttribute('from')}).
+    c('vm_id').t(vm_id).up().
+    c('action').t('connect');
+  });
+  stackvm_socket.addListener("disconnect", function(errno) {
+    xmpp.message({to:msg.getAttribute('from')}).
+    c('vm_id').t(vm_id).up().
+    c('action').t('disconnect');
+  });
+  // stackvm_socket.addListener("data", function(data) { });
+  */
 
-    /* But for now, let's use the same logic as in console.js */
+  /*
+  ** But for now, let's use the same logic as in console.js
+  */
 
-    var vm  = http.createClient(vm_map[vm_id]['port'], vm_map[vm_id]['host']);
-    //var req = vm.request('GET', '/api/console/get_update_list/
-    var req = vm.request('GET', '/api/console/get_screen');
-    req.addListener('response', function(res) {
-      var buffer = '';
-      res.addListener('data', function (chunk) {
-        buffer += chunk;
-      });
-      res.addListener('end', function () {
-        xmpp_connection.send(xmpp.message({to:msg.getAttribute('from')}).
-        c('vm_id').t(vm_id).up().
-        c('action').t('update_screen').up().
-        c('png').t(base64.encode(buffer)));
-      });
+  var vm  = http.createClient(vm_map[vm_id]['port'], vm_map[vm_id]['host']);
+  var req = vm.request('GET', '/api/console/get_screen');
+  req.addListener('response', function(res) {
+    res.setEncoding('binary');
+    var png = new buffer.Buffer(parseInt(res.headers['content-length']));
+    res.addListener('data', function (chunk) {
+      png.write(chunk, 'binary');
     });
-    req.close();
-  }
+    res.addListener('end', function () {
+      xmpp_connection.send(xmpp.message({to:msg.getAttribute('from')}).
+      c('vm_id').t(vm_id).up().
+      c('action').t('redraw_screen').up().
+      c('png').t(base64.encode(png)));
+    });
+  });
+  req.end();
 }
 
 function handler_key_down(vm_id, msg) {
   var key = msg.getChild('key').getText();
+  sys.log('got: ' + key);
   var vm  = http.createClient(vm_map[vm_id]['port'], vm_map[vm_id]['host']);
   var req = vm.request('GET', '/api/console/send_key_down/' + key);
-  req.close();
+  req.end();
 }
 
 function handler_key_up(vm_id, msg) {
   var key = msg.getChild('key').getText();
   var vm  = http.createClient(vm_map[vm_id]['port'], vm_map[vm_id]['host']);
   var req = vm.request('GET', '/api/console/send_key_up/' + key);
-  req.close();
+  req.end();
 }
 
 var handlers = {
@@ -126,15 +121,21 @@ function onMessage(msg) {
   var vm_id = msg.getChild('vm_id');
   if (vm_id) {
     vm_id = vm_id.getText();
+    if (!vm_map[vm_id]) {
+      xmpp_connection.send(
+        xmpp.message({to:msg.getAttribute('from')}).
+        c('vm_id').t(vm_id).up().
+        c('error').t("There is no VM with id '" + vm_id + "'"));
+      return;
+    }
     action = msg.getChild('action').getText();
     handler = handlers[action];
-    if (handler) {
-      handler(vm_id, msg);
-    }
-    else {
+    if (!handler) {
       sys.log("Unknown action '" + action + "' for message:");
       sys.log(msg.toString());
+      return;
     }
+    handler(vm_id, msg);
   }
   else {
     sys.log("Unknown message: ");
