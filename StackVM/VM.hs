82751efaf363baf8d4923713bf6cf516be01b3d8
James Halliday <substack@gmail.com>
2010-05-10 01:15:26 -0800

    map for active updates, but it's pretty much always the whole screen for some reason

--- StackVM/VM.hs
+++ StackVM/VM.hs
@@ -9,7 +9,7 @@ import qualified Data.ByteString as BS
 import qualified Data.ByteString.Lazy as LBS
 
 import Control.Monad (forever,forM_,join,liftM2)
-import Control.Arrow ((&&&),(***))
+import Control.Arrow (first,second,(&&&),(***))
 import Control.Applicative ((<$>))
 
 import Data.List (find)
@@ -17,13 +17,13 @@ import Data.List.Split (splitEvery)
 import Data.Word (Word8,Word32)
 
 import Control.Concurrent.MVar
-import Debug.Trace (trace)
+import qualified Data.Map as M
 
 type UpdateID = Int
 
 data VM = VM {
     vmRFB :: RFB.RFB,
-    vmUpdates :: MVar [UpdateFB],
+    vmMerged :: MVar (M.Map UpdateID UpdateFB),
     vmLatest :: MVar UpdateID
 }
 
@@ -39,12 +39,12 @@ renderPng UpdateFB{ updateImage = im } = GD.savePngByteString im
 
 newVM :: RFB.RFB -> IO VM
 newVM rfb = do
-    updates <- newMVar []
+    merged <- newMVar M.empty
     version <- newMVar 0
     
     return $ VM {
         vmRFB = rfb,
-        vmUpdates = updates,
+        vmMerged = merged,
         vmLatest = version
     }
 
@@ -54,7 +54,7 @@ updateFromImage pos size im = do
         updateImage = im,
         updatePos = pos,
         updateSize = size,
-        updateID = 0
+        updateID = 1
     }
 
 newUpdate :: RFB.RFB -> [RFB.Rectangle] -> IO UpdateFB
@@ -90,18 +90,23 @@ newUpdate rfb rects = do
         updateImage = im,
         updatePos = (n,w),
         updateSize = size,
-        updateID = 0
+        updateID = 1
     }
  
 getUpdate :: VM -> UpdateID -> IO UpdateFB
-getUpdate vm@VM{ vmUpdates = uVar } version = do
-    mUpdate <- find ((version ==) . updateID) <$> readMVar uVar
-    case mUpdate of
-        Just update -> return update
+getUpdate vm@VM{ vmMerged = mVar, vmLatest = idVar } uID = do
+    vmID <- readMVar idVar
+    mergeMap <- takeMVar mVar
+    update <- case M.lookup uID mergeMap of
+        Just u -> return u
         Nothing -> updateFromImage (0,0) size =<< RFB.getImage (vmRFB vm)
             where
                 size = RFB.fbWidth &&& RFB.fbHeight $ rfb
                 rfb = RFB.rfbFB (vmRFB vm)
+    putMVar mVar
+        $ M.insert vmID (update { updateID = vmID })
+        $ M.delete uID mergeMap
+    return update
 
 -- Overlay the imagery from u1 onto the data and image in u2.
 mergeUpdate :: UpdateFB -> UpdateFB -> IO UpdateFB
@@ -128,19 +133,20 @@ mergeUpdate u1 u2 = do
 
 updateThread :: VM -> IO ()
 updateThread vm@VM{ vmRFB = rfb, vmLatest = idVar } = forever $ do
-    let VM{ vmUpdates = updateVar } = vm
+    let VM{ vmMerged = mVar } = vm
     
     rects <- RFB.rectangles <$> RFB.getUpdate rfb
+    
+    uID <- (+1) <$> takeMVar idVar
+    putMVar idVar uID
+    
+    print . M.keys =<< readMVar mVar
     
     RFB.renderImages rfb rects
     update <- newUpdate rfb rects
     
-    uID <- takeMVar idVar
-    putMVar idVar (uID + 1)
-    
-    -- updates are tied to the latest version upon creation
-    let newestUpdate = update { updateID = uID }
-    
-    putMVar updateVar . take 5 . (newestUpdate :)
-        =<< mapM (mergeUpdate update)
-        =<< takeMVar updateVar
+    -- merged updates can live around for 20 updates maximum
+    putMVar mVar . M.fromList
+        =<< mapM (\(k,v) -> ((,) k) <$> mergeUpdate update v)
+            . filter ((> (uID - 20)) . fst) . M.toList
+        =<< takeMVar mVar
