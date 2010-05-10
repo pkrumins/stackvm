6a0857af3b1119529f1e19f722ddc08576258d8b
Peteris Krumins <peteris.krumins@gmail.com>
2010-05-11 01:43:36 +0300

    rip 2 spaces. long live 4 spaces.

--- nodejs/stackvm.js
+++ nodejs/stackvm.js
@@ -8,134 +8,134 @@ var buffer = require('buffer');
 
 // for now just a map, later lookup from database, etc.
 var vm_map = {
-  linux1: {'host': '127.0.0.1', 'port': 25900},
-  dos1:   {'host': '127.0.0.1', 'port': 25901}
+    linux1: {'host': '127.0.0.1', 'port': 25900},
+    dos1:   {'host': '127.0.0.1', 'port': 25901}
 };
 
 var vm_connection_map = {};
 
 function handler_start_vm(msg, client) {
-  if (vm_connection_map[msg.vm_id]) {
-    client.send(JSON.stringify({
-      vm_id:  msg.vm_id,
-      action: 'connected'
-    }));
-    stackvm_socket.write('update ' + msg.vm_id + ' 0\n');
-    return;
-  }
-
-  var vm_host = vm_map[msg.vm_id]['host'];
-  var vm_port = vm_map[msg.vm_id]['port'];
-
-  stackvm_socket = net.createConnection(vm_port, vm_host);
-  stackvm_socket.setTimeout(0);
-  stackvm_socket.setKeepAlive(true, 60*1000);
-  stackvm_socket.setEncoding('binary');
-  stackvm_socket.addListener('error', function(e) {
-    sys.log("VM " + msg.vm_id + " connection error: " + String(e));
-    client.send(JSON.stringify({
-      vm_id:   msg.vm_id,
-      action:  'error',
-      message: String(e)
-    }));
-  });
-  stackvm_socket.addListener('connect', function() {
-    sys.log("Connected to " + msg.vm_id);
-    client.send(JSON.stringify({
-      vm_id:  msg.vm_id,
-      action: 'connected'
-    }));
-    stackvm_socket.write('update ' + msg.vm_id + ' 0\n');
-  });
-  stackvm_socket.addListener('disconnect', function(errno) {
-    sys.log("Disconnected from " + msg.vm_id);
-    client.send(JSON.stringify({
-      vm_id:  msg.vm_id,
-      action: 'disconnected'
-    }));
-  });
-
-  var EXPECT_SIZE = 0;
-  var EXPECT_DATA = 1;
-
-  vm_connection_map[msg.vm_id] = {
-    'state':  EXPECT_SIZE
-  };
-
-  stackvm_socket.addListener('data', function(data) {
-    // for now just send the data to the client, multiplex later on client id, and
-    // keep track of update id.
-    connmap = vm_connection_map[msg.vm_id];
-    if (connmap.state == EXPECT_SIZE) {
-      var size = parseInt(data);
-      connmap.state = EXPECT_DATA;
-      connmap.buffer = new buffer.Buffer(size);
-      connmap.size = size;
-      connmap.written = 0;
+    if (vm_connection_map[msg.vm_id]) {
+        client.send(JSON.stringify({
+            vm_id:  msg.vm_id,
+            action: 'connected'
+        }));
+        stackvm_socket.write('update ' + msg.vm_id + ' 0\n');
+        return;
     }
-    else if (connmap.state == EXPECT_DATA) {
-      connmap.written += connmap.buffer.write(data, 'binary', connmap.written);
-      if (connmap.written == connmap.size) {
-        sys.log(connmap.buffer.toString());
-        client.send(connmap.buffer.toString());
-        connmap.state = EXPECT_SIZE;
-      }
-    }
-  });
-  vm_connection_map[msg.vm_id].socket = stackvm_socket;
+
+    var vm_host = vm_map[msg.vm_id]['host'];
+    var vm_port = vm_map[msg.vm_id]['port'];
+
+    stackvm_socket = net.createConnection(vm_port, vm_host);
+    stackvm_socket.setTimeout(0);
+    stackvm_socket.setKeepAlive(true, 60*1000);
+    stackvm_socket.setEncoding('binary');
+    stackvm_socket.addListener('error', function(e) {
+        sys.log("VM " + msg.vm_id + " connection error: " + String(e));
+        client.send(JSON.stringify({
+            vm_id:   msg.vm_id,
+            action:  'error',
+            message: String(e)
+        }));
+    });
+    stackvm_socket.addListener('connect', function() {
+        sys.log("Connected to " + msg.vm_id);
+        client.send(JSON.stringify({
+            vm_id:  msg.vm_id,
+            action: 'connected'
+        }));
+        stackvm_socket.write('update ' + msg.vm_id + ' 0\n');
+    });
+    stackvm_socket.addListener('disconnect', function(errno) {
+        sys.log("Disconnected from " + msg.vm_id);
+        client.send(JSON.stringify({
+            vm_id:  msg.vm_id,
+            action: 'disconnected'
+        }));
+    });
+
+    var EXPECT_SIZE = 0;
+    var EXPECT_DATA = 1;
+
+    vm_connection_map[msg.vm_id] = {
+        'state':  EXPECT_SIZE
+    };
+
+    stackvm_socket.addListener('data', function(data) {
+        // for now just send the data to the client, multiplex later on client id, and
+        // keep track of update id.
+        connmap = vm_connection_map[msg.vm_id];
+        if (connmap.state == EXPECT_SIZE) {
+            var size = parseInt(data);
+            connmap.state = EXPECT_DATA;
+            connmap.buffer = new buffer.Buffer(size);
+            connmap.size = size;
+            connmap.written = 0;
+        }
+        else if (connmap.state == EXPECT_DATA) {
+            connmap.written += connmap.buffer.write(data, 'binary', connmap.written);
+            if (connmap.written == connmap.size) {
+                sys.log(connmap.buffer.toString());
+                client.send(connmap.buffer.toString());
+                connmap.state = EXPECT_SIZE;
+            }
+        }
+    });
+    vm_connection_map[msg.vm_id].socket = stackvm_socket;
 }
 
 function handler_key_down(msg, client) {
-  vm_connection_map[msg.vm_id].socket.write('key_down ' + msg.vm_id + ' ' + msg.key + '\n');
+    vm_connection_map[msg.vm_id].socket.write('key_down ' + msg.vm_id + ' ' + msg.key + '\n');
 }
 
 function handler_key_up(msg, client) {
-  vm_connection_map[msg.vm_id].socket.write('key_up ' + msg.vm_id + ' ' + msg.key + '\n');
+    vm_connection_map[msg.vm_id].socket.write('key_up ' + msg.vm_id + ' ' + msg.key + '\n');
 }
 
 function handler_pointer(msg, client) {
-  to_write = 'pointer ' + msg.vm_id + ' ' + msg.x + ' ' + msg.y + ' ' + msg.mask + '\n';
-  vm_connection_map[msg.vm_id].socket.write(to_write);
+    to_write = 'pointer ' + msg.vm_id + ' ' + msg.x + ' ' + msg.y + ' ' + msg.mask + '\n';
+    vm_connection_map[msg.vm_id].socket.write(to_write);
 }
 
 // TODO: just call handler_*(msg, client) for the given action
 var handlers = {
-  'start_vm': function(msg, client) { handler_start_vm(msg, client); },
-  'redraw_screen': function(msg, client) { handler_redraw_screen(msg, client); },
-  'key_down': function(msg, client) { handler_key_down(msg, client); },
-  'key_up': function(msg, client) { handler_key_up(msg, client); },
-  'pointer': function(msg, client) { handler_pointer(msg, client); },
+    'start_vm': function(msg, client) { handler_start_vm(msg, client); },
+    'redraw_screen': function(msg, client) { handler_redraw_screen(msg, client); },
+    'key_down': function(msg, client) { handler_key_down(msg, client); },
+    'key_up': function(msg, client) { handler_key_up(msg, client); },
+    'pointer': function(msg, client) { handler_pointer(msg, client); },
 }
 
 function handle_message(msg, client) {
-  if (!msg.vm_id) {
-    sys.log("Unknown message:");
-    sys.log(JSON.stringify(msg));
-    return;
-  }
-  if (!vm_map[msg.vm_id]) {
-    sys.log("Unknown VM ID:");
-    sys.log(JSON.stringify(msg));
-    client.send(JSON.stringify({
-      error: "There is no VM with id '" + msg.vm_id + "'"
-    }));
-    return;
-  }
-  if (!msg.action) {
-    sys.log("Unknown action:");
-    sys.log(JSON.stringify(msg));
-    client.send(JSON.stringify({
-      error: "No action specified."
-    }));
-    return;
-  }
-  handler = handlers[msg.action];
-  if (!handler) {
-    sys.log("Unknown handler.");
-    sys.log(JSON.stringify(msg));
-    return;
-  }
-  handler(msg, client);
+    if (!msg.vm_id) {
+        sys.log("Unknown message:");
+        sys.log(JSON.stringify(msg));
+        return;
+    }
+    if (!vm_map[msg.vm_id]) {
+        sys.log("Unknown VM ID:");
+        sys.log(JSON.stringify(msg));
+        client.send(JSON.stringify({
+            error: "There is no VM with id '" + msg.vm_id + "'"
+        }));
+        return;
+    }
+    if (!msg.action) {
+        sys.log("Unknown action:");
+        sys.log(JSON.stringify(msg));
+        client.send(JSON.stringify({
+            error: "No action specified."
+        }));
+        return;
+    }
+    handler = handlers[msg.action];
+    if (!handler) {
+        sys.log("Unknown handler.");
+        sys.log(JSON.stringify(msg));
+        return;
+    }
+    handler(msg, client);
 }
 
 exports.handle_message = handle_message;
--- nodejs/static/js/vm.js
+++ nodejs/static/js/vm.js
@@ -1,228 +1,228 @@
 var VM_Manager = (function() {
-  var vms = {};
-  var active_vm = null;
+    var vms = {};
+    var active_vm = null;
 
-  return {
-    add: function(vm) {
-      vms[vm.vm_id] = vm;
-    },
-    get: function(id) {
-      return vms[id];
-    },
-    del: function(id) {
-      delete vms[id];
-    },
-    get_active_vm: function() {
-      return active_vm;
-    },
-    set_active_vm: function(vm) {
-      active_vm = vm;
-    },
-  }
+    return {
+        add: function(vm) {
+             vms[vm.vm_id] = vm;
+        },
+        get: function(id) {
+             return vms[id];
+        },
+        del: function(id) {
+             delete vms[id];
+        },
+        get_active_vm: function() {
+             return active_vm;
+        },
+        set_active_vm: function(vm) {
+             active_vm = vm;
+        },
+    }
 })();
 
 function VM_Event_Handler(vm) {
-  this.vm = vm;
+    this.vm = vm;
 
-  this.error = function(msg) {
-    console.log("Error: " + msg.error);
-  }
-
-  this.connected = function(msg) {
-    $('.loading', vm.win).hide();
-  }
-
-  function png_img(png) {
-    // TODO: use MHTML for IE
-    return $('<img>').attr('src', 'data:image/png;base64,' + png);
-  }
-
-  function cleanup_images(except) {
-    $('.console img', vm.win)
-      .not(except)
-      .remove();
-  }
-
-  function update_screen(img, x, y, w, h) {
-    var console = $('.console', vm.win);
-    if (h > vm.win.height() + 22) {
-      vm.win.height(h+22); // 22 to account for window's .title.
+    this.error = function(msg) {
+        console.log("Error: " + msg.error);
     }
-    if (w > vm.win.width()) {
-      vm.win.width(w);
+
+    this.connected = function(msg) {
+        $('.loading', vm.win).hide();
     }
-    img.css({
-      position: 'absolute',
-      left:     x,
-      top:      y,
-      width:    w,
-      height:   h
-    });
-    console.append(img);
-  }
 
-  this.redraw_screen = function(msg) {
-    var img = png_img(msg.png);
-    update_screen(img, 0, 0, parseInt(msg.width), parseInt(msg.height));
-    cleanup_images(img);
-  }
+    function png_img(png) {
+        // TODO: use MHTML for IE
+        return $('<img>').attr('src', 'data:image/png;base64,' + png);
+    }
 
-  this.update_screen = function(msg) {
-    var img = png_img(msg.png);
-    update_screen(img, parseInt(msg.x), parseInt(msg.y),
-      parseInt(msg.width), parseInt(msg.height));
-  }
+    function cleanup_images(except) {
+        $('.console img', vm.win)
+             .not(except)
+             .remove();
+    }
 
-  this.disconnected = function(msg) {
-    console.log('vm ' + msg.vm_id + ' disconnected');
-  }
+    function update_screen(img, x, y, w, h) {
+        var console = $('.console', vm.win);
+        if (h > vm.win.height() + 22) {
+             vm.win.height(h+22); // 22 to account for window's .title.
+        }
+        if (w > vm.win.width()) {
+             vm.win.width(w);
+        }
+        img.css({
+             position: 'absolute',
+             left:     x,
+             top:      y,
+             width:    w,
+             height:   h
+        });
+        console.append(img);
+    }
+
+    this.redraw_screen = function(msg) {
+        var img = png_img(msg.png);
+        update_screen(img, 0, 0, parseInt(msg.width), parseInt(msg.height));
+        cleanup_images(img);
+    }
+
+    this.update_screen = function(msg) {
+        var img = png_img(msg.png);
+        update_screen(img, parseInt(msg.x), parseInt(msg.y),
+             parseInt(msg.width), parseInt(msg.height));
+    }
+
+    this.disconnected = function(msg) {
+        console.log('vm ' + msg.vm_id + ' disconnected');
+    }
 }
 
 function VM_Event_Emitter(vm) {
-  this.start_vm = function() {
-    Connection.send_msg({
-      vm_id:  vm.vm_id,
-      action: 'start_vm'
-    });
-  }
+    this.start_vm = function() {
+        Connection.send_msg({
+             vm_id:  vm.vm_id,
+             action: 'start_vm'
+        });
+    }
 
-  this.redraw_screen = function() {
-    Connection.send_msg({
-      vm_id:  vm.vm_id,
-      action: 'redraw_screen'
-    });
-  }
+    this.redraw_screen = function() {
+        Connection.send_msg({
+             vm_id:  vm.vm_id,
+             action: 'redraw_screen'
+        });
+    }
 
-  this.send_key_down = function(key_code) {
-    Connection.send_msg({
-      vm_id:  vm.vm_id,
-      action: 'key_down',
-      key:    String(KeyMapper.get_key_sym(key_code))
-    });
-  }
+    this.send_key_down = function(key_code) {
+        Connection.send_msg({
+             vm_id:  vm.vm_id,
+             action: 'key_down',
+             key:    String(KeyMapper.get_key_sym(key_code))
+        });
+    }
 
-  this.send_pointer = function(x,y,mask) {
-    Connection.send_msg({
-      vm_id: vm.vm_id,
-      action: 'pointer',
-      x : String(x),
-      y : String(y),
-      mask : String(mask)
-    });
-  }
+    this.send_pointer = function(x,y,mask) {
+        Connection.send_msg({
+             vm_id: vm.vm_id,
+             action: 'pointer',
+             x : String(x),
+             y : String(y),
+             mask : String(mask)
+        });
+    }
 
-  this.send_key_up = function(key_code) {
-    Connection.send_msg({
-      vm_id:  vm.vm_id,
-      action: 'key_up',
-      key:    String(KeyMapper.get_key_sym(key_code))
-    });
-  }
+    this.send_key_up = function(key_code) {
+        Connection.send_msg({
+             vm_id:  vm.vm_id,
+             action: 'key_up',
+             key:    String(KeyMapper.get_key_sym(key_code))
+        });
+    }
 }
 
 function VM(vm_id) {
-  this.vm_id  = vm_id;
-  this.win = null;
-  this.title = null;
-  var event_emitter = new VM_Event_Emitter(this);
+    this.vm_id  = vm_id;
+    this.win = null;
+    this.title = null;
+    var event_emitter = new VM_Event_Emitter(this);
 
-  function create_window() {
-    var win = $('<div>')
-      . addClass('window')
-      . attr('tabindex', 0)
-      . data('vm_id', vm_id)
-      . width(400)
-      . height(200)
-      . draggable();
+    function create_window() {
+        var win = $('<div>')
+             . addClass('window')
+             . attr('tabindex', 0)
+             . data('vm_id', vm_id)
+             . width(400)
+             . height(200)
+             . draggable();
 
-    var title = $('<div>').addClass('title');
-    title.append($('<div>').addClass('text').text(vm_id));
-    title.append(
-      $('<div>').addClass('buttons').append(
-        $('<div>').addClass('refresh').append(
-          $('<img>').attr('src', '/img/refresh.png').click(
-            function(ev) {
-              event_emitter.redraw_screen();
-            }
-          )
-        )
-      )
-    ).append($('<div>').addClass('clear'));
-    win.append(title);
+        var title = $('<div>').addClass('title');
+        title.append($('<div>').addClass('text').text(vm_id));
+        title.append(
+             $('<div>').addClass('buttons').append(
+               $('<div>').addClass('refresh').append(
+                 $('<img>').attr('src', '/img/refresh.png').click(
+                   function(ev) {
+                     event_emitter.redraw_screen();
+                   }
+                 )
+               )
+             )
+        ).append($('<div>').addClass('clear'));
+        win.append(title);
 
-    var con = $('<div>').addClass('console');
-    con.append(
-      $('<div>').addClass('loading').css({
-        'margin-top': (200-20)/2-10 + 'px',
-        'text-align': 'center',
-      }).
-      text('Loading ' + vm_id + '...')
-    );
-    win.append(con);
-    
-    win.mouseover(function(ev) {
-      focus(win);
-    });
-    
-    win.mouseout(function(ev) {
-      unfocus(win);
-    });
-    
-    var mouse_mask = 0;
-    
-    win.mousemove(function(ev) {
-      var x = ev.pageX - win.offset().left - 3;
-      var y = ev.pageY - win.offset().top - 23;
-      console.log([x,y].map(String).join(','));
-      event_emitter.send_pointer(x,y,mouse_mask);
-    });
-    
-    win.mousedown(function(ev) {
-      mouse_mask = 1;
-      event_emitter.send_pointer(ev.pageX,ev.pageY,mouse_mask);
-    });
-    
-    win.mouseup(function(ev) {
-      mouse_mask = 0;
-      event_emitter.send_pointer(ev.pageX,ev.pageY,mouse_mask);
-    });
-    
-    win.keydown(function(ev) {
-      event_emitter.send_key_down(ev.keyCode);
-    });
-    
-    win.keyup(function(ev) {
-      event_emitter.send_key_up(ev.keyCode);
-    });
+        var con = $('<div>').addClass('console');
+        con.append(
+             $('<div>').addClass('loading').css({
+               'margin-top': (200-20)/2-10 + 'px',
+               'text-align': 'center',
+             }).
+             text('Loading ' + vm_id + '...')
+        );
+        win.append(con);
+        
+        win.mouseover(function(ev) {
+             focus(win);
+        });
+        
+        win.mouseout(function(ev) {
+             unfocus(win);
+        });
+        
+        var mouse_mask = 0;
+        
+        win.mousemove(function(ev) {
+             var x = ev.pageX - win.offset().left - 3;
+             var y = ev.pageY - win.offset().top - 23;
+             console.log([x,y].map(String).join(','));
+             event_emitter.send_pointer(x,y,mouse_mask);
+        });
+        
+        win.mousedown(function(ev) {
+             mouse_mask = 1;
+             event_emitter.send_pointer(ev.pageX,ev.pageY,mouse_mask);
+        });
+        
+        win.mouseup(function(ev) {
+             mouse_mask = 0;
+             event_emitter.send_pointer(ev.pageX,ev.pageY,mouse_mask);
+        });
+        
+        win.keydown(function(ev) {
+             event_emitter.send_key_down(ev.keyCode);
+        });
+        
+        win.keyup(function(ev) {
+             event_emitter.send_key_up(ev.keyCode);
+        });
 
-    $('#content').append(win);
-    return win;
-  }
+        $('#content').append(win);
+        return win;
+    }
 
-  function focus(win) {
-    VM_Manager.set_active_vm(win);
-    win.focus();
-    $('.title', win).addClass("active-title");
-  }
+    function focus(win) {
+        VM_Manager.set_active_vm(win);
+        win.focus();
+        $('.title', win).addClass("active-title");
+    }
 
-  function unfocus(win) {
-    VM_Manager.set_active_vm(null);
-    $('.focusremover').focus();
-    $('.title', win).removeClass("active-title");
-  }
+    function unfocus(win) {
+        VM_Manager.set_active_vm(null);
+        $('.focusremover').focus();
+        $('.title', win).removeClass("active-title");
+    }
 
-  this.focus = function() {
-    focus(this.win);
-  }
+    this.focus = function() {
+        focus(this.win);
+    }
 
-  this.unfocus = function() {
-    unfocus(this.win);
-  }
+    this.unfocus = function() {
+        unfocus(this.win);
+    }
 
-  this.run = function() {
-    this.win = create_window();
-    Connection.add_event_handler(new VM_Event_Handler(this));
-    event_emitter.start_vm();
-  }
+    this.run = function() {
+        this.win = create_window();
+        Connection.add_event_handler(new VM_Event_Handler(this));
+        event_emitter.start_vm();
+    }
 }
 
