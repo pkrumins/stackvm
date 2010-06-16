eaabd056867169a48c6f2750892c2d28b42c6524
Peteris Krumins <peteris.krumins@gmail.com>
2010-06-16 20:02:41 +0300

    screenshots now completely work

--- bin/screenshots.js
+++ bin/screenshots.js
@@ -5,9 +5,21 @@ var http = require('http');
 var fs = require('fs');
 var sys = require('sys');
 
+var screenshotDir = "./static/screenshots";
+
+function randomFileName () {
+    var fileName = '';
+    for (var i = 0; i<32; i++) {
+        fileName += String.fromCharCode(Math.random()*26 + 97);
+    }
+    return fileName;
+}
+
 DNode({
     screenshot : DNode.async(function (png, f) {
-        fs.writeFile('moo.png', png, 'binary', function (err) {
+        var fileName = randomFileName() + '.png';
+        var fullPath = screenshotDir + '/' + fileName;
+        fs.writeFile(fullPath, png, 'binary', function (err) {
             if (err) {
                 f({
                     status : 'error',
@@ -17,14 +29,33 @@ DNode({
             }
             f({
                 status : 'success',
-                filename : 'moo.png'
+                fileName : fileName
             });
         });
     })
 }).listen(9200);
 
 http.createServer(function (req, res) {
-    // serve images
+    if (!/^\/[a-z]{32}\.png$/.test(req.url)) {
+        res.writeHead(400);
+        res.end();
+        return;
+    }
+
+    var fileName = screenshotDir + req.url;
+    sys.log(fileName);
+    res.writeHead(200, { 'Content-Type': 'image/png' });
+    fs.readFile(fileName, 'binary', function (err, data) {
+        if (err) {
+            var msg = "Error reading " + fileName + ": " + err.toString();
+            sys.log(msg);
+            res.write(msg);
+            res.end();
+            return;
+        }
+        res.write(data, 'binary');
+        res.end();
+    });
 }).listen(9201);
 
 sys.log('Screenshot DNode server running on port 9200');
