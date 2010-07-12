65f87f3bcb0612adde2aa639a41302dec8bb6b6e
Peteris Krumins <peteris.krumins@gmail.com>
2010-07-12 12:58:31 +0300

    started moving stackvm to dnode

--- bin/webstack.js
+++ bin/webstack.js
@@ -3,10 +3,9 @@
 require.paths.unshift(__dirname + '/..');
 
 var sys = require('sys');
-var socketio = require('socket.io');
-
 var webserver = require('lib/webserver').webserver;
 var VM = require('lib/vm').VM;
+var DNode = require('dnode').DNode;
 
 var port = Number(process.argv[2]) || 9000;
 
@@ -21,59 +20,38 @@ var vms = {
 webserver.listen(port, '0.0.0.0');
 sys.log("Webserver running at 0.0.0.0:" + port + ".");
 
-socketio.listen(webserver, {
-    // flashsockets require that pesky 843 port, so forget those
-    transports : 'websocket htmlfile xhr-multipart xhr-polling'.split(/\s+/),
-    
-    onClientConnect : function(client) {
-        var client_ip = client.request.connection.remoteAddress;
-        sys.log("Client from " + client_ip + " connected.");
+DNode(function (client) {
+    keyDown : function (key) {
+        vm.keyDown(key);
     },
-    
-    onClientDisconnect : function(client) {
-        var client_ip = client.request.connection.remoteAddress;
-        sys.log("Client from " + client_ip + " disconnected.");
+    keyUp : function (key) {
+        vm.keyUp(key);
     },
-    
-    onClientMessage : function(msg, client) {
-        var vm = vms[msg.vmId];
-        var f = {
-            list : function () {
-                var acc = [];
-                for (var id in vms) acc.push(vms[id]);
-                client.send(acc);
-            },
-            attach : function () {
-                vm.attach(client);
-                vm.requestRedrawScreen();
-            },
-            detach : function () {
-                vm.detach(client);
-            },
-            keyDown : function () {
-                vm.keyDown(parseInt(msg.key, 10));
-            },
-            keyUp : function () {
-                vm.keyUp(parseInt(msg.key, 10));
-            },
-            pointer : function () {
-                vm.sendPointer(parseInt(msg.x, 10), parseInt(msg.y, 10),
-                    parseInt(msg.mask, 10));
-            },
-            redrawScreen : function () {
-                vm.requestRedrawScreen();
-            },
-            takeScreenshot : function () {
-                vm.takeScreenshot();
-            },
-            startScreencast : function () {
-                vm.startScreencast();
-            },
-            stopScreencast : function () {
-                vm.stopScreencast();
-            }
-        }[msg.action];
-        if (f) f();
+    pointer : function (x, y, mask) {
+        vm.sendPointer(x, y, mask);
+    },
+    redrawScreen : function () {
+        vm.requestRedrawScreen();
+    },
+    takeScreenshot : function () {
+        vm.takeScreenshot();
+    },
+    startScreencast : function () {
+        vm.startScreencast();
+    },
+    stopScreencast : function () {
+        vm.stopScreencast();
+    },
+    attach : function () {
+        vm.attach(client);
+        vm.requestRedrawScreen();
+    },
+    detach : function () {
+        vm.detach(client);
     }
+}).listen({
+    protocol : 'socket.io',
+    server : webserver,
+    transports : 'websocket xhr-multipart xhr-polling htmlfile'.split(/\s+/),
 });
 
