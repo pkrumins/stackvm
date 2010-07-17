521653578673a02fa877cf077127fc39e583d730
James Halliday <substack@gmail.com>
2010-07-17 08:19:49 -0800

    now with dnode authentication

--- bin/webstack.js
+++ bin/webstack.js
@@ -3,55 +3,40 @@
 require.paths.unshift(__dirname + '/..');
 
 var sys = require('sys');
-var webserver = require('lib/webserver').webserver;
-var VM = require('lib/vm').VM;
+var fs = require('fs');
 var DNode = require('dnode').DNode;
+var crypto = require('crypto');
+
+var webserver = require('lib/webserver').webserver;
+var Session = require('lib/session').Session;
 
 var port = Number(process.argv[2]) || 9000;
-
-var vms = {
-    linux : new VM({
-        id : 'linux',
-        image : __dirname + '/../vms/linux-0.2.img',
-        port : 5900,
-    }),
-};
 
 webserver.listen(port, '0.0.0.0');
 sys.log("Webserver running at 0.0.0.0:" + port + ".");
 
-DNode(function (client) {
-    keyDown : function (key) {
-        vm.keyDown(key);
-    },
-    keyUp : function (key) {
-        vm.keyUp(key);
-    },
-    pointer : function (x, y, mask) {
-        vm.sendPointer(x, y, mask);
-    },
-    redrawScreen : function () {
-        vm.requestRedrawScreen();
-    },
-    takeScreenshot : function () {
-        vm.takeScreenshot();
-    },
-    startScreencast : function () {
-        vm.startScreencast();
-    },
-    stopScreencast : function () {
-        vm.stopScreencast();
-    },
-    attach : function () {
-        vm.attach(client);
-        vm.requestRedrawScreen();
-    },
-    detach : function () {
-        vm.detach(client);
-    }
+DNode(function (client,conn) {
+    this.authenticate = function (user,pass,cb) {
+        if (user.match(/[^\w-]/)) {
+            cb(null);
+            return;
+        }
+        var passFile = __dirname + '/../users/' + user + '/passwd';
+        var hash = new crypto.Hash('sha512').update(pass).digest('hex');
+        fs.readFile(passFile, function (err,fileHash) {
+            cb(hash == fileHash
+                ? new Session({
+                    client : client,
+                    connection : conn,
+                    user : user
+                })
+                : null
+            );
+        });
+    };
 }).listen({
     protocol : 'socket.io',
     server : webserver,
     transports : 'websocket xhr-multipart xhr-polling htmlfile'.split(/\s+/),
-});
+}).listen(9001);
 
