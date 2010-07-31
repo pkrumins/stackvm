262f9cb50bc8c3012f71c20258fb893de20f111d
Peteris Krumins <peteris.krumins@gmail.com>
2010-07-31 06:17:44 +0300

    factor out QemuManager and VMWareManager

--- bin/manager.js
+++ bin/manager.js
@@ -3,6 +3,7 @@
 
 var sys = require('sys');
 var proc = require('child_process');
+var net = require('net');
 var DNode = require('dnode');
 
 var sqlite = require('sqlite');
@@ -10,22 +11,123 @@ var db = sqlite.openDatabaseSync(__dirname + '/../data/vm.db');
 
 var crypto = require('crypto');
 
+var QemuManager = {
+    isAlive : function (row, f) {
+        try {
+            process.kill(row.pid,0);
+            f(true);
+        } catch (e) {
+            f(false);
+        }
+    },
+
+    spawn : function (user, vm, procs, f) {
+        var port = 5900;
+        for (; Object.keys(procs).some(function (uid) {
+            return 'localhost:' + port in procs[uid]
+        }); port++);
+
+        var hostPort = 'localhost:' + port; // localhost for now
+
+        sys.puts('firing up qemu on ' + hostPort);
+
+        var qemu = proc.spawn('qemu', [
+            '-vnc', ':' + (port - 5900),
+            __dirname + '/../users/' + user.name + '/vms/' + vm.filename
+        ]);
+
+        qemu.on('exit', function () {
+            db.query('delete from processes where host = ?', [hostPort]);
+            delete procs[user.id][hostPort];
+            sys.puts('qemu on ' + hostPort + ' died');
+        });
+        
+        db.query(
+            'insert into processes (vm,engine,host,pid) values (?,?,?,?)',
+            [ vm.id, 'qemu', hostPort, qemu.pid ],
+            function (r) {
+                if (r.rowsAffected == 1) {
+                    var vmProc = {
+                        vm : vm.id,
+                        engine : 'qemu',
+                        host : hostPort,
+                        pid : qemu.pid,
+                    };
+                    procs[user.id][hostPort] = vmProc;
+                    f(vmProc);
+                }
+                else { f(null) }
+            }
+        );
+    },
+
+    kill : function (vmProc, f) {
+        process.kill(vmProc.pid);
+        db.query(
+            'delete from processes where host = ?', [vmProc.host],
+            function (r) { f(r.rowsAffected == 1) }
+        );
+    },
+};
+
+var VMWareManager  = {
+    isAlive : function (row, f) {
+        var hostPort = row.host.split(':');
+        var stream = net.createConnection(hostPort[1], hostPort[0]);
+        stream.setTimeout(1000);
+        stream.on('connect', function () {
+            stream.end();
+            f(true);
+        });
+        stream.on('error', function () {
+            f(false);
+        });
+    },
+
+    spawn : function (user, vm, procs, f) {
+        db.query(
+            'insert into processes (vm,engine,host,pid) values (?,?,?,?)',
+            [ vm.id, 'vmware', vm.host, 'NULL' ],
+            function (r) {
+                if (r.rowsAffected == 1) {
+                    var vmProc = {
+                        vm : vm.id,
+                        engine : 'vmware',
+                        host : vm.host,
+                        pid : 0
+                    };
+                    procs[user.id][vm.host] = vmProc;
+                    f(vmProc);
+                }
+                else { f(null) }
+            }
+        );
+    },
+
+    kill : function (user, host, f) {
+        db.query(
+            'delete from processes where host = ?', [vmProc.host],
+            function (r) { f(r.rowsAffected == 1) }
+        );
+    },
+};
+
+var managers = {
+    qemu : QemuManager,
+    vmware : VMWareManager
+};
+
 db.query('select processes.*, vms.owner from processes, vms '
 + 'where processes.vm = vms.id', function (rows) {
     var procs = {};
     rows.forEach(function (row) {
-        var alive = false;
-        try {
-            process.kill(row.pid,0);
-            alive = true;
-        } catch (e) {
-            db.query('delete from processes where pid = ?', [row.pid]);
-        }
-        if (alive) {
-            if (!(row.owner in procs))
-                procs[row.owner] = {};
-            procs[row.owner][row.port] = row;
-        }
+        managers[row.engine].isAlive(row, function (alive) {
+            if (alive) {
+                if (!(row.owner in procs))
+                    procs[row.owner] = {};
+                procs[row.owner][row.host] = row;
+            }
+        });
     });
     
     DNode(function (client,conn) {
@@ -45,7 +147,7 @@ function Manager(params) {
     
     self.virtualMachines = function (user, f) {
         db.query(
-            'select id, name, filename from vms where owner = ? ', [user.id],
+            'select id, name, engine, filename, host from vms where owner = ? ', [user.id],
             function (rows) {
                 var vms = {};
                 rows.forEach(function (row) { vms[row.id] = row });
@@ -62,68 +164,27 @@ function Manager(params) {
         var user = params.user;
         var vm = params.vm;
         var engine = params.engine; // qemu, vmware, vbox
-        
+
         if (!(user.id in procs)) procs[user.id] = {};
-        
-        var port = 5900;
-        for (; Object.keys(procs).some(function (uid) {
-            return port in procs[uid]
-        }); port++);
-        
-        sys.puts('firing up ' + engine + ' on port ' + port);
-        
-        if (engine == 'qemu') {
-            var qemu = proc.spawn('qemu', [
-                '-vnc', ':' + (port - 5900),
-                __dirname + '/../users/' + user.name + '/vms/' + vm.filename
-            ]);
-            
-            qemu.on('exit', function () {
-                db.query('delete from processes where port = ?', [port]);
-                delete procs[user.id][port];
-                sys.puts('qemu on port ' + port + ' died');
-            });
-            
-            db.query(
-                'insert into processes (vm,engine,port,pid) values (?,?,?,?)',
-                [ vm.id, engine, port, qemu.pid ],
-                function (r) {
-                    if (r.rowsAffected == 1) {
-                        var proc = {
-                            vm : vm.id,
-                            engine : engine,
-                            port : port,
-                            pid : qemu.pid,
-                        };
-                        procs[user.id][port] = proc;
-                        f(proc);
-                    }
-                    else { f(null) }
-                }
-            );
-        }
-        else { f(null) }
+        managers[engine].spawn(user, vm, procs, f);
     };
     
-    self.kill = function (user, port, f) {
-        if (port in procs[user.id]) {
-            process.kill(procs[user.id][port].pid);
-            delete procs[user.id][port];
-            db.query(
-                'delete from processes where port = ?', [port],
-                function (r) { f(r.rowsAffected == 1) }
-            );
+    self.kill = function (user, host, f) {
+        if (host in procs[user.id]) {
+            var vmProc = procs[user.id][host]
+            managers[vmProc.engine].kill(vmProc, f)
+            delete procs[user.id][host];
         }
         else {
             console.log(
-                user.id + '/' + port + ' not in procs: '
+                user.id + '/' + host + ' not in procs: '
                 + sys.inspect(procs)
             );
         }
     };
     
     self.restart = function (params, f) {
-        self.kill(params.user, params.vm.port, function () {
+        self.kill(params.user, params.vm.host, function () {
             self.spawn(params, f);
         });
     };
