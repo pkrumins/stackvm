77b34748f38f25c41ad77ba1e30acf0534c0a986
James Halliday <substack@gmail.com>
2010-09-06 14:33:13 -0800

    merge manager stuff into webstack, but spawn doesn't propagate events yet

--- bin/webstack.js
+++ bin/webstack.js
@@ -1,40 +1,53 @@
 #!/usr/bin/env node
 // This program serves stackvm webpages and stackvm streams.
 require.paths.unshift(__dirname + '/..');
+require.paths.unshift(__dirname + '/../lib');
 
 var sys = require('sys');
 var fs = require('fs');
-var DNode = require('dnode').DNode;
 var crypto = require('crypto');
 
-var webserver = require('lib/webserver').webserver;
-var Session = require('lib/session');
+var DNode = require('dnode');
+var Hash = require('traverse/hash');
 
 var port = Number(process.argv[2]) || 9000;
-
+var webserver = require('lib/webserver').webserver;
 webserver.listen(port, '0.0.0.0');
-sys.log("Webserver running at 0.0.0.0:" + port + ".");
+console.log('Webserver running at http://localhost:' + port);
 
-DNode.connect(9077, function (manager) {
-    DNode(function (client,conn) {
-        this.authenticate = function (name,pass,cb) {
-            manager.authenticate(name, pass, function (user) {
-                if (user) {
-                    var session = new Session({
-                        client : client,
-                        connection : conn,
-                        user : user,
-                        manager : manager,
-                    });
-                    cb(session);
-                }
-                else cb(null);
-            });
-        };
-    }).listen({
-        protocol : 'socket.io',
-        server : webserver,
-        transports : 'websocket xhr-multipart xhr-polling htmlfile'
-            .split(/\s+/),
-    }).listen(9001);
+var Session = require('lib/session');
+var User = require('models/user');
+
+var cookies = {};
+
+var jsonFile = __dirname + '/../data/users.json';
+var json = JSON.parse(fs.readFileSync(jsonFile));
+var users = Hash.map(json, function (user, name) {
+    user.processes = {};
+    user.name = name;
+    return user;
+});
+
+DNode(function (client,conn) {
+    this.authenticate = function (params, cb) {
+        if (params.user && params.pass) {
+            var hash = User.hash(params.pass);
+            var name = params.user.toLowerCase();
+            var user = users[name];
+            if (users.hasOwnProperty(name) && hash == user.hash) {
+                var session = new Session({
+                    client : client,
+                    connection : conn,
+                    user : user,
+                });
+                cb(session);
+            }
+            else {
+                cb(null);
+            }
+        }
+    };
+}).listen({
+    server : webserver,
+    transports : 'websocket xhr-multipart xhr-polling htmlfile'.split(/\s+/),
 });
--- static/js/session.js
+++ static/js/session.js
@@ -1,4 +1,4 @@
-function Session (user, pass, cb) {
+function Session (params, cb) {
     var contacts = new EventEmitter;
     DNode.expose(contacts, 'emit');
     
@@ -6,7 +6,7 @@ function Session (user, pass, cb) {
     DNode.expose(processes, 'emit');
     
     DNode().connect(function (remote, conn) {
-        remote.authenticate(user, pass, function (account) {
+        remote.authenticate(params, function (account) {
             cb(account ? new UI(account) : null);
         });
     });
