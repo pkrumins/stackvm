fc34a14ed5cae4bbab4f9df8842c1f03b83c67ad
James Halliday <substack@gmail.com>
2010-09-08 00:08:34 -0800

    now using express

--- bin/stackvm.js
+++ bin/stackvm.js
@@ -1,18 +1,17 @@
 #!/usr/bin/env node
 // This program serves stackvm webpages and stackvm streams.
+
 require.paths.unshift(__dirname + '/..');
 require.paths.unshift(__dirname + '/../lib');
 
-var sys = require('sys');
-var fs = require('fs');
-var crypto = require('crypto');
-
-var DNode = require('dnode');
-var Hash = require('traverse/hash');
-
 var port = Number(process.argv[2]) || 9000;
-var webserver = require('lib/webserver').webserver;
-webserver.listen(port, '0.0.0.0');
+
+var express = require('express');
+var app = express.createServer();
+app.use(express.staticProvider(__dirname + '/../static'));
+app.get('/js/dnode.js', require('dnode/web').route());
+
+app.listen(port, '0.0.0.0');
 console.log('Webserver running at http://localhost:' + port);
 
 var Session = require('lib/session');
@@ -20,10 +19,12 @@ var User = require('models/user');
 
 var cookies = {};
 
+// todo: real database goes here
 var jsonFile = __dirname + '/../data/users.json';
-var json = JSON.parse(fs.readFileSync(jsonFile));
+var json = JSON.parse(require('fs').readFileSync(jsonFile));
 var users = User.fromHashes(json);
 
+var DNode = require('dnode');
 DNode(function (client, conn) {
     this.authenticate = function (params, cb) {
         if (params.user && params.pass) {
@@ -51,6 +52,6 @@ DNode(function (client, conn) {
         }
     };
 }).listen({
-    server : webserver,
+    server : app,
     transports : 'websocket xhr-multipart xhr-polling htmlfile'.split(/\s+/),
 });
