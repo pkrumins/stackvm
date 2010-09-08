16a1a31daeb66b20db9fba26cfe9abe07e026021
James Halliday <substack@gmail.com>
2010-09-08 14:35:51 -0800

    fixes to server.js to handle being in the project root

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
 #!/usr/bin/env node
 // This program serves stackvm webpages and stackvm streams.
 
-require.paths.unshift(__dirname + '/..');
-require.paths.unshift(__dirname + '/../lib');
+require.paths.unshift(__dirname + '/.');
+require.paths.unshift(__dirname + '/lib');
 
 var port = Number(process.argv[2]) || 9000;
 
 var express = require('express');
 var app = express.createServer();
-app.use(express.staticProvider(__dirname + '/../static'));
+app.use(express.staticProvider(__dirname + '/static'));
 app.get('/js/dnode.js', require('dnode/web').route());
 
 app.listen(port, '0.0.0.0');
@@ -20,7 +20,7 @@ var User = require('models/user');
 var cookies = {};
 
 // todo: real database goes here
-var jsonFile = __dirname + '/../data/users.json';
+var jsonFile = __dirname + '/data/users.json';
 var json = JSON.parse(require('fs').readFileSync(jsonFile));
 var users = User.fromHashes(json);
 
