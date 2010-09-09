a1ac75886977fa810d2fef6de561e41ad15879ef
James Halliday <substack@gmail.com>
2010-09-08 20:36:34 -0800

    now using nStore to load users instead of a json file

--- server.js
+++ server.js
@@ -13,33 +13,24 @@ require.paths.unshift(__dirname + '/lib');
 var Service = require('lib/service');
 var User = require('models/user');
 
-var sessions = nStore(__dirname + '/data/sessions.db');
-
 var app = express.createServer();
 app.use(express.staticProvider(__dirname + '/static'));
 app.get('/js/dnode.js', require('dnode/web').route());
 app.listen(port, '0.0.0.0');
 
+var sessions = nStore(__dirname + '/data/sessions.db');
+
 nStore(__dirname + '/data/users.db').all(
     function () { return true },
     function (err, hashes, metas) {
-        var users = User.fromHashes(Hash.zip(metas, hashes));
-console.log('== users ==');
-console.dir(users);
+        var keys = metas.map(function (x) { return x.key });
+        var users = User.fromHashes(Hash.zip(keys, hashes));
         var service = Service(users);
         DNode(service).listen({
             server : app,
             transports : 'websocket xhr-multipart xhr-polling htmlfile'
                 .split(/\s+/),
         });
-            console.log('Webserver running at http://localhost:' + port);
-        
+        console.log('StackVM running at http://localhost:' + port);
     }
 );
-
-/*
-var fs = require('fs');
-var json = fs.readFileSync(__dirname + '/data/users.json');
-var users = User.fromHashes(JSON.parse(json));
-*/
-
