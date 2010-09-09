a7e23cfdfe3484ea7c023c8e84c2a534bdc89c2b
James Halliday <substack@gmail.com>
2010-09-08 20:16:57 -0800

    refactored to load users from nStore

--- server.js
+++ server.js
@@ -1,57 +1,45 @@
 #!/usr/bin/env node
-// This program serves stackvm webpages and stackvm streams.
+
+var express = require('express');
+var nStore = require('nStore');
+var DNode = require('dnode');
+var Hash = require('traverse/hash');
+
+var port = Number(process.argv[2]) || 9000;
 
 require.paths.unshift(__dirname + '/.');
 require.paths.unshift(__dirname + '/lib');
 
-var port = Number(process.argv[2]) || 9000;
+var Service = require('lib/service');
+var User = require('models/user');
 
-var express = require('express');
+var sessions = nStore(__dirname + '/data/sessions.db');
+
 var app = express.createServer();
 app.use(express.staticProvider(__dirname + '/static'));
 app.get('/js/dnode.js', require('dnode/web').route());
-
 app.listen(port, '0.0.0.0');
-console.log('Webserver running at http://localhost:' + port);
 
-var Session = require('lib/session');
-var User = require('models/user');
+nStore(__dirname + '/data/users.db').all(
+    function () { return true },
+    function (err, hashes, metas) {
+        var users = User.fromHashes(Hash.zip(metas, hashes));
+console.log('== users ==');
+console.dir(users);
+        var service = Service(users);
+        DNode(service).listen({
+            server : app,
+            transports : 'websocket xhr-multipart xhr-polling htmlfile'
+                .split(/\s+/),
+        });
+            console.log('Webserver running at http://localhost:' + port);
+        
+    }
+);
 
-var cookies = {};
+/*
+var fs = require('fs');
+var json = fs.readFileSync(__dirname + '/data/users.json');
+var users = User.fromHashes(JSON.parse(json));
+*/
 
-// todo: real database goes here
-var jsonFile = __dirname + '/data/users.json';
-var json = JSON.parse(require('fs').readFileSync(jsonFile));
-var users = User.fromHashes(json);
-
-var DNode = require('dnode');
-DNode(function (client, conn) {
-    this.authenticate = function (params, cb) {
-        if (params.user && params.pass) {
-            var hash = User.hash(params.pass);
-            var name = params.user.toLowerCase();
-            var user = users[name];
-            if (users.hasOwnProperty(name) && hash == user.hash) {
-                var session = new Session(user, conn);
-                user.sessions[conn.id] = session;
-                
-                if (user.connections == 0) user.emit('online');
-                user.connections ++;
-                
-                conn.on('end', function () {
-                    user.connections --;
-                    delete user.sessions[conn.id];
-                    if (user.connections == 0) user.emit('offline');
-                });
-                
-                cb(session.attach(conn));
-            }
-            else {
-                cb(null);
-            }
-        }
-    };
-}).listen({
-    server : app,
-    transports : 'websocket xhr-multipart xhr-polling htmlfile'.split(/\s+/),
-});
