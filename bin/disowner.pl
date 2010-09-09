4381531898011ab4c588bcfe98109026de9d98b2
James Halliday <substack@gmail.com>
2010-09-09 00:40:03 -0800

    disown a process, but it still dies when stackvm catches a SIGINT

--- bin/disowner.pl
+++ bin/disowner.pl
@@ -1,10 +1,11 @@
 #!/usr/bin/env perl
 use POSIX qw/WNOHANG/;
 use Time::HiRes qw/sleep/;
+$|++;
 
 $SIG{CHLD} = 'IGNORE';
 if (my $pid = fork) {
-    print "$pid\n";
+    print $pid;
 }
 else {
     sleep 0.1; # time for the print to finish up
