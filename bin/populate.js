7488fcda2cabad109d33a12817ee077c30dc604a
Peteris Krumins <peteris.krumins@gmail.com>
2010-09-14 15:24:55 +0300

    move populate to supermarket

--- bin/populate.js
+++ bin/populate.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 
 var fs = require('fs');
-var nStore = require('nStore');
+var Store = require('supermarket');
 var Hash = require('traverse/hash');
 
 if (process.argv.length <= 2) {
@@ -15,11 +15,11 @@ if (process.argv.length <= 2) {
 
 var json = JSON.parse(fs.readFileSync(process.argv[2]));
 
-var users = nStore(__dirname + '/../data/users.db');
-
-Hash(json).forEach(function (user, name) {
-    users.save(name, user, function (err) {
-        if (err) throw err;
+Store(__dirname + '/../data/users.db', function (err, db) {
+    Hash(json).forEach(function (user, name) {
+        db.set(name, user, function (err) {
+            if (err) throw err;
+        });
     });
 });
 
