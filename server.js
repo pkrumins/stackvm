14e3fd8e9c03148b5d5d682809bcaf43521c43fc
Peteris Krumins <peteris.krumins@gmail.com>
2010-09-14 23:37:06 +0300

    uses Cart instead of nStoreSession now

--- server.js
+++ server.js
@@ -1,20 +1,20 @@
 #!/usr/bin/env node
 
 var express = require('express');
-var nStore = require('nStore');
+var Store = require('supermarket');
 var DNode = require('dnode');
 var Hash = require('traverse/hash');
 
 var port = Number(process.argv[2]) || 9000;
 
-var nStoreSession = require('nStoreSession');
+var Cart = require('cart');
 
 var app = express.createServer();
 app.use(express.staticProvider(__dirname + '/static'));
 app.use(express.cookieDecoder());
 app.use(express.bodyDecoder());
 app.use(express.session({
-    store : new nStoreSession({ dbFile : __dirname + '/data/sessions.db' }),
+    store : new Cart({ dbFile : __dirname + '/data/sessions.db' }),
     secret : 'todo: set this in the stackvm site config with cli.js'
 }));
 
@@ -31,18 +31,17 @@ app.configure('production', function () {
 
 app.get('/js/dnode.js', require('dnode/web').route());
 
-var db = { user : nStore(__dirname + '/data/users.db') };
+var db = { user : Store(__dirname + '/data/users.db') };
 require('./lib/web')(app, db);
 
 app.listen(port, '0.0.0.0');
 
 var Service = require('./lib/service');
-db.user.all(function (err, hashes, metas) {
-    Service(db, function (service) {
-        DNode(service).listen(app, {
-            transports : 'websocket xhr-multipart xhr-polling htmlfile'
-                .split(/\s+/),
-        });
-        console.log('StackVM running at http://localhost:' + port);
+Service(db, function (service) {
+    DNode(service).listen(app, {
+        transports : 'websocket xhr-multipart xhr-polling htmlfile'
+            .split(/\s+/),
     });
+    console.log('StackVM running at http://localhost:' + port);
 });
+
