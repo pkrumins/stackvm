338a7f420f320588cc3a0c82a97d0505ea5376dd
James Halliday <substack@gmail.com>
2010-09-15 18:05:33 -0800

    reconnection on the right side of the connection, works in chrome

--- server.js
+++ server.js
@@ -39,7 +39,8 @@ app.listen(port, '0.0.0.0');
 var Service = require('./lib/service');
 Service(db, function (service) {
     DNode(service).listen(app, {
-        ping : 5000, timeout : 5000,
+        ping : 1000,
+        timeout : 1000,
         transports : 'websocket xhr-multipart xhr-polling htmlfile'
             .split(/\s+/),
     });
--- static/js/session.js
+++ static/js/session.js
@@ -1,9 +1,28 @@
 function Session (cb) {
-    DNode().connect(function (remote, conn) {
-        remote.authenticate(function (err, account) {
-            if (err) cb(err);
-            else cb(null, new UI(account));
-        });
-    });
+    DNode().connect(
+        { ping : 2000, timeout : 100 },
+        function (remote, conn) {
+            remote.authenticate(function (err, account) {
+                if (err) cb(err);
+                else cb(null, new UI(account));
+            });
+            
+            function reconnect () {
+                if (window.console) console.log('reconnecting');
+                conn.reconnect(3000, function f (err) {
+                    if (err) {
+                        if (window.console) console.log(err);
+                        reconnect();
+                    }
+                    else {
+                        if (window.console) console.log('reconnected');
+                    }
+                });
+            }
+            
+            conn.on('timeout', reconnect);
+            conn.on('end', reconnect);
+        }
+    );
 }
 
