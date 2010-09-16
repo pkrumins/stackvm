f4461fafc53f2e82f508115e4ddd08256bf4f504
James Halliday <substack@gmail.com>
2010-09-15 23:04:44 -0800

    the robit can share a vm with ycdemo now

--- bin/robit.js
+++ bin/robit.js
@@ -1,18 +1,48 @@
 #!/usr/bin/env node
 var DNode = require('dnode');
+var Hash = require('traverse/hash');
+
 DNode.connect(9090, function (remote) {
     remote.authenticate('robit', 'yc', function (err, account) {
         if (err) { throw err; return }
         
         var ycdemo = account.contacts.ycdemo;
         
-        ycdemo.subscribe(function (sub) {
-            sub.on('online', function () {
-                console.log('ycdemo is online');
-                setTimeout(function () {
-                    ycdemo.message('Hello friend! I am the stackvM robot.');
-                }, 3000);
+        var simpleLinux = account.disks['linux-0.2.img'];
+        simpleLinux.subscribe(function (sub) {
+            sub.on('spawn', function (proc) {
+                ycdemo.subscribe(function (sub) {
+                    sub.on('online', script.bind({}, ycdemo, proc));
+                });
             });
         });
+        simpleLinux.spawn('qemu');
     });
 });
+
+function script (yc, proc) {
+    console.log('ycdemo is online');
+    
+    var items = [
+        [ 2000, function () {
+            yc.message('Hello! I am the StackVM robot.');
+        } ],
+        [ 2000, function () {
+            yc.message("I'm here to show off some nifty "
+                + 'features, like this chat!');
+        } ],
+        [ 2000, function () {
+            yc.message("Or here, I'll share a VM with you.");
+        } ],
+        [ 1500, function () {
+            yc.share('process', proc.address, {
+                ycdemo : { input : true, view : true }
+            });
+        } ],
+    ];
+    
+    (function next (yc) {
+        var ix = items.shift();
+        if (ix) setTimeout(function () { ix[1](); next() }, ix[0]);
+    })();
+}
