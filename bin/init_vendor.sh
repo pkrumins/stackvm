cdce593bfb7a620cfc97b268c775e06836a4a3ce
Peteris Krumins <peteris.krumins@gmail.com>
2010-09-21 04:43:53 +0300

    improve init_vendor a bit

--- bin/init_vendor.sh
+++ bin/init_vendor.sh
@@ -10,24 +10,40 @@ test $# -gt 0 && dst=${1%/} || {
     exit 1
 }
 
+mkdir -p "$dst"
+
 jquery="http://code.jquery.com/jquery-1.4.2.min.js"
 jquery_ui="http://jqueryui.com/download/jquery-ui-1.8.5.custom.zip"
 jquery_wheel="http://github.com/brandonaaron/jquery-mousewheel.git"
 
+function jQuery {
+    wget $jquery -O "$dst/${jquery##*/}"
+}
+
+function jQueryUi {
+    wget $jquery_ui -O "/tmp/${jquery_ui##*/}"
+    jquery_ui_dir="/tmp/jquery-ui-$RANDOM"
+    mkdir $jquery_ui_dir
+    unzip "/tmp/${jquery_ui##*/}" -d $jquery_ui_dir
+    cp "$jquery_ui_dir/js/jquery-ui-1.8.5.custom.min.js" "$dst"
+    rm -rf $jquery_ui_dir
+}
+
+function jQueryMouseWheel {
+    jquery_wheel_dir="/tmp/jquery-wheel-$RANDOM"
+    git clone "http://github.com/brandonaaron/jquery-mousewheel.git" $jquery_wheel_dir
+    cp "$jquery_wheel_dir/jquery.mousewheel.js" "$dst"
+    rm -rf $jquery_wheel_dir
+}
+
 echo "Installing jQuery"
-wget $jquery -O "$dst/${jquery##*/}"
+jQuery >/dev/null
 
 echo "Installing jQuery-ui"
-wget $jquery_ui -O "/tmp/${jquery_ui##*/}"
-jquery_ui_dir="/tmp/jquery-ui-$RANDOM"
-mkdir $jquery_ui_dir
-unzip "/tmp/${jquery_ui##*/}" -d $jquery_ui_dir
-cp "$jquery_ui_dir/js/jquery-ui-1.8.5.custom.min.js" "$dst"
-rm -rf $jquery_ui_dir
+jQueryUi >/dev/null
 
 echo "Installing jQuery-mousewheel"
-jquery_wheel_dir="/tmp/jquery-wheel-$RANDOM"
-git clone "http://github.com/brandonaaron/jquery-mousewheel.git" $jquery_wheel_dir
-cp "$jquery_wheel_dir/jquery.mousewheel.js" "$dst"
-rm -rf $jquery_wheel_dir
+jQueryMouseWheel >/dev/null
+
+echo "Done"
 
