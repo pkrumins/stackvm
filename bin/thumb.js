6974ba62bb8a1adf691a1853e555fee4b2089497
James Halliday <substack@gmail.com>
2010-09-25 15:47:08 -0800

    thumb events tied to processes, not disks >_<, works

--- bin/thumb.js
+++ bin/thumb.js
@@ -7,11 +7,12 @@ DNode.connect(9090, function (remote, conn) {
         if (err) { throw err; return }
         
         var simpleLinux = account.disks['linux-0.2.img'];
-        simpleLinux.subscribe(function (sub) {
-            sub.on('thumb', function (filename) {
-                console.log(simpleLinux.filename + '/' + filename);
+        simpleLinux.spawn('qemu', function (proc) {
+            proc.subscribe(function (sub) {
+                sub.on('thumb', function (filename) {
+                    console.log(simpleLinux.filename + '/' + filename);
+                });
             });
         });
-        simpleLinux.spawn('qemu');
     });
 });
