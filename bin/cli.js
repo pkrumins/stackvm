728b63e9c3ab26de609c5455dc6d0747c6a18b9a
Peteris Krumins <peteris.krumins@gmail.com>
2010-09-25 06:40:30 +0300

    added hasQemu to test for qemu

--- bin/cli.js
+++ bin/cli.js
@@ -1,6 +1,17 @@
 #!/usr/bin/env node
 
 var argv = require('optimist').argv;
+var spawn = require('child_process').spawn;
 
 // not sure how to proceed
+
+
+function hasQemu (cb) {
+    var which = spawn('which', ['qemu']);
+    which.on('exit', function (code) {
+        cb(code==0);
+    });
+}
+
+exports.hasQemu = hasQemu;
 
