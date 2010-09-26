f55c806caf85c8c6a60ed4839c3ae0d2f2e5a51a
James Halliday <substack@gmail.com>
2010-09-25 21:51:04 -0800

    swapped out where hash gets loaded and now errors out at startup

--- server.js
+++ server.js
@@ -2,12 +2,14 @@
 
 var express = require('express');
 var Store = require('supermarket');
+var Cart = require('cart');
 var DNode = require('dnode');
 var Hash = require('traverse/hash');
 
-var port = Number(process.argv[2]) || 9000;
+var User = require('./lib/models/user');
+var web = require('./lib/web');
 
-var Cart = require('cart');
+var port = Number(process.argv[2]) || 9000;
 
 var app = express.createServer();
 app.use(express.staticProvider(__dirname + '/static'));
@@ -31,22 +33,26 @@ app.configure('production', function () {
 
 app.get('/js/dnode.js', require('dnode/web').route());
 
-var db = { user : Store({ filename : __dirname + '/data/users.db', json : true }) };
-require('./lib/web')(app, db);
-
-app.listen(port, '0.0.0.0');
-
-var Service = require('./lib/service');
-Service(db, function (service) {
-    DNode(service)
-        .listen(app, {
-            ping : 1000,
-            timeout : 1000,
-            transports : 'websocket xhr-multipart xhr-polling htmlfile'
-                .split(/\s+/),
-        })
-        .listen(9090)
-    ;
-    console.log('StackVM running at http://localhost:' + port);
+Store({
+    filename : __dirname + '/data/users.db' //, json : true
+}).all(function (err, names, metas) {
+console.log('all!');
+    var users = User.fromHashes(Hash.zip(names, metas));
+    
+    web(app, users);
+    
+    var Service = require('./lib/service');
+    Service(users, function (service) {
+        DNode(service)
+            .listen(app, {
+                ping : 1000,
+                timeout : 1000,
+                transports : 'websocket xhr-multipart xhr-polling htmlfile'
+                    .split(/\s+/),
+            })
+            .listen(9090)
+        ;
+        app.listen(port, '0.0.0.0');
+        console.log('StackVM running at http://localhost:' + port);
+    });
 });
-
