1798057046096c537cd301f4ea9d2b5ca261d9cf
James Halliday <substack@gmail.com>
2010-09-28 13:27:15 -0800

    thumbnails in the web side, thumb event isn't getting caught though

--- static/js/site.js
+++ static/js/site.js
@@ -1,11 +1,45 @@
 $(document).ready(function () {
     DNode.connect(function (remote) {
         remote.session(function (err, account) {
-            if (account) {
-                Hash(account.disks).forEach(function (disk, filename) {
-                    console.log(filename);
-                });
-            }
+            if (!err) Hash(account.disks).forEach(registerDisk);
         });
     });
 });
+
+function registerDisk (disk, filename) {
+    var diskElem = $('<div>')
+        .addClass('proc')
+        .text(filename)
+        .appendTo($('#disks'))
+    ;
+    
+    function registerProc (proc) {
+        var thumb = $('<div>');
+        function makeImg () {
+            var uri = '/disks/' + filename + '/' + proc.address + '/thumbnail';
+            return $('<img>').attr('src', uri).width(200).height(150);
+        }
+        
+        var procElem = $('<div>')
+            .addClass('proc')
+            .append(makeImg)
+            .appendTo(diskElem)
+        ;
+        proc.subscribe(function (sub) {
+            sub.on('exit', function () {
+                thumb.remove();
+            });
+            
+            sub.on('thumb', function () {
+                thumb.find('img').remove();
+                thumb.append(makeImg());
+            });
+        });
+    }
+    
+    Hash(disk.processes).forEach(registerProc);
+    
+    disk.subscribe(function (sub) {
+        sub.on('spawn', registerProc);
+    });
+}
