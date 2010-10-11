e543946722b97d8736b4c7187a461a26b7439b76
James Halliday <substack@gmail.com>
2010-10-11 12:30:52 +0000

    swapped in cwd(), still using users.json though

--- bin/server.js
+++ bin/server.js
@@ -18,7 +18,7 @@ app.use(express.staticProvider(__dirname + '/../static'));
 app.use(express.cookieDecoder());
 app.use(express.bodyDecoder());
 app.use(express.session({
-    store : new Cart({ dbFile : __dirname + '/../data/sessions.db' }),
+    store : new Cart({ dbFile : process.cwd() + '/data/sessions.db' }),
     secret : 'todo: set this in the stackvm site config with cli.js'
 }));
 
@@ -35,8 +35,9 @@ app.configure('production', function () {
 
 app.get('/js/dnode.js', require('dnode/web').route());
 
+// TODO: use supermarket here
 var users = User.fromHashes(
-    JSON.parse(fs.readFileSync(__dirname + '/../data/users.json'))
+    JSON.parse(fs.readFileSync(process.cwd() + '/data/users.json'))
 );
 
 Web(app, users);
