2af572ba37c808d3d7cc2d6d815e91db4bc24aad
James Halliday <substack@gmail.com>
2010-10-11 08:36:41 +0000

    deploy in cli.js now creates directories and updates local.json

--- bin/cli.js
+++ bin/cli.js
@@ -2,32 +2,56 @@
 var fs = require('fs');
 var sys = require('sys');
 var deploy = require('../lib/setup/deploy');
+var Hash = require('traverse/hash');
 
 var argv = require('optimist')
     .usage(fs.readFileSync(__dirname + '/../doc/cli.txt', 'utf8'))
     .check(function (args) { if (args._.length == 0) throw '' })
     .argv;
 
+function updateJSON (file, f) {
+    fs.readFile(file, function (err, buf) {
+        var data = JSON.parse(buf.toString());
+        var out = JSON.stringify(f(data));
+        fs.write(file, out);
+    });
+}
+
 var cmd = argv._.shift();
 var action = {
     deploy : function () {
-        if (argv._.length == 0) {
-            throw 'Usage: deploy [directory] {options}'
+        if (argv._.length < 2) {
+            throw 'Usage: deploy [name] [directory] {options}';
         }
-        deploy(argv._[0], argv, function (err) {
-            if (err) console.error('\n    !!! ' + err + '\n');
-        });
-    }
+        
+        sys.print('Deploying StackVM to ' + argv._[1] + '...    ');
+        deploy(Hash.merge(argv, {
+            name : argv._[0],
+            base : argv._[1],
+            done : function (err) {
+                if (err) {
+                    console.log('failed');
+                    console.error('\n    !!! '
+                        + (err.stack ? err.stack : err) + '\n'
+                    );
+                }
+                else console.log('ok');
+            },
+        }));
+    },
+    start : function () {
+        var name = argv._.length ? argv._[0] : 'main';
+    },
+    stop : function () {
+    },
 }[cmd];
 
 if (action === undefined) {
     console.error('Undefined command ' + sys.inspect(cmd));
 }
 else {
-    try {
-        action();
-    }
+    try { action() }
     catch (err) {
-        console.error(err);
+        console.error('\n' + (err.stack ? err.stack : err));
     }
 }
